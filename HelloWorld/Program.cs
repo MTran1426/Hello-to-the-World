6dbef1d105d51910aad499d3f8fb5e9cd1d00ea0
Melvin Tran <melvin.tran1426@gmail.com>
2021-01-06 16:29:40 -0700

    First change

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; // I want direct acess to the static members of the Console class
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0) // I have one or more strings supplied to my program
+            {
+                foreach (string word in args)
+                {
+                    WriteLine($"Hello {word}");
+                }
+            }
+            else
+            {
+                //Help Information
+                ForegroundColor = ConsoleColor.Green; // Change the forground color
+                WriteLine(@"Hello World
+======== =======
+
+Usage: .\HelloWorld [names]
+
+where
+     [names] is a Space-separated list of names");
+                ResetColor(); //Resets the color
+            }
+        }
+    }
+}
